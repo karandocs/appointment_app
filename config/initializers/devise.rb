872c23b8735b2f937f67837d5a9df23668349b7a
karan <kirubakarann7@gmail.com>
2017-10-24 03:36:08 +0530

    Database changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,7 @@ Devise.setup do |config|
 
   config.navigational_formats = [:json]
   config.mailer_sender = "karan@gmail.com"
-  config.omniauth :facebook, "App ID", "App Secret", callback_url: "http://localhost:3000/users/auth/facebook/callback"
-  # config.secret_key = '86f5e054f11e423d7ad19e58171baeed17176adb9d6287e92459cf4ef5f0fc0a63e20320600957025c8bf49132c6dedb6e02a3f71e6eb6df7d844c4bfcab3bb2'
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "https://evening-savannah-35894.herokuapp.com/users/auth/facebook/callback", scope: 'email', info_fields: 'email'
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], callback_url: "https://evening-savannah-35894.herokuapp.com/auth/google_oauth2/callback"
+    # config.secret_key = '86f5e054f11e423d7ad19e58171baeed17176adb9d6287e92459cf4ef5f0fc0a63e20320600957025c8bf49132c6dedb6e02a3f71e6eb6df7d844c4bfcab3bb2'
 end
--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
-  client_options: {
-    site: 'https://graph.facebook.com/v2.10',  # this is the example API version
-    authorize_url: "https://www.facebook.com/v2.10/dialog/oauth"
-  }
-  provider :google_oauth2, "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"
-end
